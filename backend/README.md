c519c7a7d2925e6eb12ad26eb62a6093faf9ead8
Thomas Bennett <tbenz9@gmail.com>
2020-02-07 22:50:34 +0000

    initial commit with backend MVP

---
+++ backend/README.md
@@ -0,0 +1,102 @@
+## Install nodejs and npm using apt
+```
+#> sudo apt install nodejs npm
+```
+
+## Install node dependencies
+```
+#> npm install express sqlite3 body-parser flags
+```
+
+## Set up the sqlite3 database
+```
+#> sqlite3 dn.db
+sqlite> create table FILES ( fileLink TEXT PRIMARY KEY, fileName TEXT, fileType TEXT, fileCategory TEXT, fileUploadedTimestamp INT, fileUploadedUser INT, file
+Description TEXT, fileRating INT, fileSize INT );
+sqlite> create table COMMENTS ( commentID INTEGER PRIMARY KEY AUTOINCREMENT, fileLink TEXT, commentTimestamp INTEGER, commentUser INTEGER, commentBody TEXT, c
+ommentRating INTEGER );
+```
+
+## To run the server
+```
+#> node server.js --debug
+```
+
+## To test the server
+```
+#> curl http://localhost:8081/files
+#> curl -X POST -H "Content-Type: application/json" -d @example.json http://localhost:8081/files
+```
+
+# API Documentation
+## To retrieve all entries in the Skynet link database (GET request)
+```
+#> curl http://localhost:8081/files
+```
+Sample response:
+```
+[
+  {
+    "fileLink": "AABZjrS1ZT6Q2K-CcXKKE78M2j5KEJSEaCltcNi_wPE3OA",
+    "fileName": "vivaldi-stable_2.10.1745.23-1_amd64.deb",
+    "fileType": "deb",
+    "fileDescription": "Vivaldi Stable Web Browser for Debian based Systems."
+  },
+  {
+    "fileLink": "XADyBtepOn4N9_cILGYD5issx269K-uRdFBye6FRVia2XQ",
+    "fileName": "2014 Sia Whitepaper",
+    "fileType": "PDF",
+    "fileDescription": "The 2014 Sia whitepaper written by Nebulous"
+  }
+]
+```
+
+## To retrieve a single entry with all the details (GET request)
+```
+#> curl http://localhost:8081/files?fileLink=AABZjrS1ZT6Q2K-CcXKKE78M2j5KEJSEaCltcNi_wPE3OA
+```
+Sample response:
+```
+{
+  "fileLink": "AABZjrS1ZT6Q2K-CcXKKE78M2j5KEJSEaCltcNi_wPE3OA",
+  "fileName": "vivaldi-stable_2.10.1745.23-1_amd64.deb",
+  "fileType": "deb",
+  "fileCategory": "application",
+  "fileUploadedTimestamp": 1581111921,
+  "fileUploadedUser": "anonymous",
+  "fileDescription": "Vivaldi Stable Web Browser for Debian based Systems.",
+  "fileRating": null,
+  "fileSize": 64724924
+}
+```
+
+## To delete a single entry (GET request)
+```
+#> curl http://localhost:8081/files/delete?fileLink=AABZjrS1ZT6Q2K-CcXKKE78M2j5KEJSEaCltcNi_wPE3OA
+```
+Sample response:
+```
+No JSON response, but a 200 status should be returned.
+```
+
+## To add a single new entry (POST request)
+```
+#> curl -X POST -H "Content-Type: application/json" -d @example1.json http://localhost:8081/files
+```
+Sample data in `example1.json`
+```
+{
+    "fileLink": "XADyBtepOn4N9_cILGYD5issx269K-uRdFBye6FRVia2XQ",
+    "fileName": "2014 Sia Whitepaper",
+    "fileType": "PDF",
+    "fileCategory": "ebook",
+    "fileUploadedTimestamp": 1581111845,
+    "fileUploadedUser": "anonymous",
+    "fileSize": 134568,
+    "fileDescription": "The 2014 Sia whitepaper written by Nebulous"
+}
+```
+Sample response:
+```
+No JSON response, but a 200 status should be returned
+```
---
+++ backend/server.js
@@ -0,0 +1,111 @@
+const body_parser = require('body-parser');
+const express = require('express');
+const flags = require('flags');
+const sqlite3 = require('sqlite3').verbose();
+var app = express();
+var fs = require("fs");
+
+app.use(body_parser.json());
+
+flags.defineBoolean('debug');
+flags.parse();
+
+if (flags.get('debug')) {
+	console.log('Running in debug mode');
+	var debug = true;
+}
+
+if (debug) { console.log('debug shorthand') }
+
+function connectDB() {
+    let db = new sqlite3.Database('./dn.db', (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        if (debug) {console.log('Connected to the datanation database.');}
+    });
+	return db;
+}
+
+function closeDB(db) {
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        if (debug) {console.log('Close the database connection.');}
+    });
+}
+
+// files POST
+app.post('/files', function(req, res) {
+    if (debug) {console.log(req.body);}
+
+	let db = connectDB();
+
+    db.run(`INSERT INTO FILES(fileLink,fileName,fileType,fileCategory,fileUploadedTimestamp,fileUploadedUser,fileSize,fileDescription) VALUES(?,?,?,?,?,?,?,?)`, [req.body.fileLink, req.body.fileName, req.body.fileType, req.body.fileCategory, req.body.fileUploadedTimestamp, req.body.fileUploadedUser, req.body.fileSize, req.body.fileDescription], function(err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        // get the last insert id
+        if (debug) {console.log(`A row has been inserted with rowid ${this.lastID}`);}
+        res.sendStatus(200)
+    });
+
+    closeDB(db);
+})
+
+// files DELETE
+app.get('/files/delete', function(req, res) {
+    if (debug) {console.log(req.body);}
+
+	let db = connectDB();
+
+    db.run(`DELETE FROM FILES WHERE fileLink=?`, [req.query.fileLink], function(err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        // get the last insert id
+        if (debug) {console.log(`A row has been deleted with rowid ${this.lastID}`);}
+        res.sendStatus(200)
+    });
+
+    closeDB(db);
+})
+
+// files GET
+app.get('/files', function(req, res) {
+    let db = connectDB();
+    if (req.query.fileLink) {
+        db.serialize(() => {
+            db.get(`SELECT * FROM FILES WHERE fileLink=?`, [req.query.fileLink], (err, row) => {
+                if (err) {
+                    console.error(err.message);
+                }
+                if (debug) {console.log("Someone Requested a single row");}
+                res.send(row);
+            });
+        });
+
+        console.log('Requesting a single file')
+    } else {
+
+        db.serialize(() => {
+            db.all(`SELECT fileLink,fileName,fileType,fileDescription FROM FILES`, (err, rows) => {
+                if (err) {
+                    console.error(err.message);
+                }
+                if (debug) {console.log("Someone Requested all rows");}
+                res.send(rows);
+            });
+        });
+
+    }
+    closeDB(db);
+    })
+
+// start the server
+var server = app.listen(8081, function() {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Skylink repo app listening at http://%s:%s", host, port)
+})
